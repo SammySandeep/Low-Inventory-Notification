ff0881f6f1e91226baf8880799aa39038a933632
shreya <shreya.b@kloctechnologies.co.in>
2021-04-01 20:21:36 +0530

    feat: added css files and view for home and shop setting

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( nav_style.css )
+Rails.application.config.assets.precompile += %w( table_style.css )
+Rails.application.config.assets.precompile += %w( button_style.css )
+Rails.application.config.assets.precompile += %w( form_style.css )

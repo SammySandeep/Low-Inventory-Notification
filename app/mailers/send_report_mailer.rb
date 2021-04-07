de65a1d99cda4bf9db84f205e62b9df44b6ac35f
hasan <hasan.ali@kloctechnologies.co.in>
2021-04-07 16:03:33 +0530

    feat: low stock alert

--- app/mailers/send_report_mailer.rb
+++ app/mailers/send_report_mailer.rb
@@ -1,11 +1,12 @@
 class SendReportMailer < ApplicationMailer
 
-    def notify(admins:, recipients: 0, target_obj_key:)
+    def notify(admin:, recipients:, target_obj_key:)
+        @url =  "#{ENV['DOWNLOAD_LINK']}#{target_obj_key.gsub(" ","$")}"
         
         if recipients.empty?
-            mail to: admins.join(","), subject: "These items will be out of stock soon." 
+            mail to: "#{admin}", subject: "These items will be out of stock soon." 
         else
-            mail to: admins.join(","), cc: recipients.join(","), subject: "These items will be out of stock soon."
+            mail to: "#{admin}", cc: recipients.join(","), subject: "These items will be out of stock soon."
         end
 
     end

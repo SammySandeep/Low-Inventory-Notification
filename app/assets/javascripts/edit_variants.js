ff7997792b5bc83877124b5c5d195aa751f8d0db
shreya <shreya.b@kloctechnologies.co.in>
2021-04-11 10:35:52 +0530

    wip

--- app/assets/javascripts/edit_variants.js
+++ app/assets/javascripts/edit_variants.js
@@ -1,38 +1,27 @@
-var x = [];
-tds = document.getElementsByTagName('td');
-for(var i=3;i<tds.length;i=i+4){
-    x.push(tds[i]);
+function changetd(tdele){
+
+        tdele.onclick = function() {
+        var $this = $(this);
+        var id = this.id;
+        var $input = $('<input>', {
+            value: $this.text(),
+            type: 'text',
+            blur: function() {
+            $this.text(this.value);
+            },
+            keyup: function(e) {
+            if (e.which === 13)
+             {
+                Rails.ajax({
+                    url: "/variants",
+                    type: "put",
+                    data: "",
+                    success: function(data) {},
+                    error: function(data) {}
+                  })
+             }
+            }
+        }).appendTo( $this.empty() ).focus();
+    };
 }
 
-for(var j=0;j<x.length;j++){
-x[j].onclick = function() {
-    var $this = $(this);
-    var id = this.id;
-    // console.log(id);
-    var $input = $('<input>', {
-        value: $this.text(),
-        type: 'text',
-        blur: function() {
-           $this.text(this.value);
-        },
-        keyup: function(e) {
-           if (e.which === 13){
-               var value = this.value;
-               console.log(value);
-               console.log(id);
-            Rails.ajax({
-                url: "/variants",
-                type: "PUT",
-                data: {id,value},
-                success: function(data) {
-                    console.log(data);
-                },
-                error: function(data) {}
-              })
-           }
-           $input.blur();
-           
-        }
-    }).appendTo( $this.empty() ).focus();
-};
-}

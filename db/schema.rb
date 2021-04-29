36051a6b44bbd3b8eb318b6f0510030f2b6fad95
shreya <shreya.b@kloctechnologies.co.in>
2021-04-29 21:03:39 +0530

    feat:shop setting ajax edit and delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_100704) do
+ActiveRecord::Schema.define(version: 2021_04_27_062349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "emails", force: :cascade do |t|
     t.text "email"
-    t.boolean "is_admin", default: false
     t.boolean "is_active", default: true
     t.integer "shop_setting_id"
     t.datetime "created_at", null: false

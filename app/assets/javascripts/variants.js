5999e9cc7a8d6d1a65b9cbb282af149de1c27402
shreya <shreya.b@kloctechnologies.co.in>
2021-06-07 21:17:50 +0530

    feat: cross button for edit variants

--- app/assets/javascripts/variants.js
+++ app/assets/javascripts/variants.js
@@ -20,14 +20,32 @@ function editIconclicked(editElement){
     var thresholdInputTd = $(previousTd).prev();
     var prevTr = $(thresholdInputTd).closest('tr');
     var variantId = $(prevTr).attr('id'); 
+    var thresholdTdValue = $(thresholdInputTd).text();
     $(thresholdInputTd).replaceWith(function () {
-        return `<input class=form-control-sm min="1" style='display:inline;width:90%;margin-top:10px;margin-bottom:10px;border-color:#2D2B75;' type=number value=${parseInt(thresholdInputTd.text())} >`
+        return `<div class="input-group"><input class=form-control-sm min="1" style='display:inline;width:80%;margin-top:10px;margin-bottom:10px;border-color:#2D2B75;' type=number value=${parseInt(thresholdInputTd.text())} > <span class="input-group-btn">
+        <button class="cross-btn" type="submit" onclick=crossButtonPressed(${variantId},${thresholdTdValue});>
+            <i class="fa fa-times"></i>
+        </button>
+      </span>
+    </div> `
     });
 
     $(editElement).replaceWith(function(){
         return `<i class="fa fa-check" aria-hidden="true" onclick=updateIconPressed(${variantId});></i>`
     });
 }
+    function crossButtonPressed(variantId,thresholdTdValue){
+        var variantTr = document.getElementById(variantId);  
+        var variantDivElement = $(variantTr).find("div"); 
+        $(variantDivElement).replaceWith(function () {
+            return `<td class="variant-threshold-text" onclick="updateIconPressed(this);">${thresholdTdValue}</td>`;
+        });    
+        var editelem = $(variantTr).find("i"); 
+        $(editelem).replaceWith(function(){
+            return `<i class="fa fa-pencil-square-o" aria-hidden="true" style="font-size:20px;" onclick="editIconclicked(this);"></i>`
+        });
+
+    }
 
 // https://stackoverflow.com/questions/62864900/rails-strong-params-not-seeing-params-in-ajax-post-request/62866069#62866069
 // Check above link to understand different ways of passing data with ajax

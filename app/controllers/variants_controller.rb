3c0bea1d55afc680e56504470313c200c6744c6e
shreya <shreya.b@kloctechnologies.co.in>
2021-06-28 12:33:20 +0530

    fix: passing messages through controller

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -16,9 +16,9 @@ class VariantsController < ApplicationController
 
   def export_csv
     ExportCsvJob.perform_later(shop_id: current_shop.id)
-    @export_csv_success_message = "Please wait while the products are being emailed as a CSV file.This will take few minutes!"
+    export_csv_success_message = "Please wait while the products are being emailed as a CSV file.This will take few minutes!"
     respond_to do |format|
-      format.js
+      format.js { render "export_csv.js.erb", :locals => {:export_csv_success_message => export_csv_success_message} }
     end 
   end
 
@@ -27,25 +27,25 @@ class VariantsController < ApplicationController
 
   def update_csv_threshold
     @import_csv_successful = ImportCsvService.new(shop_id: current_shop.id, file_path: params[:file].path).execute
-    @import_csv_success_message = "Variants Threshold successfully updated!"
-    @import_csv_error_message = "There was an error updating Variants Threshold. Please refer to the csv file format on the Home page."
+    import_csv_success_message = "Variants Threshold successfully updated!"
+    import_csv_error_message = "There was an error updating Variants Threshold. Please refer to the csv file format on the Home page."
     respond_to do |format|
-      format.js
+      format.js { render "update_csv_threshold.js.erb", :locals => {:import_csv_success_message => import_csv_success_message, :import_csv_error_message => import_csv_error_message} }
     end
   end
 
   def update
     global_threshold = current_shop.shop_setting.global_threshold
     @current_variant_threshold = @variant.threshold
-    @update_threshold_success_message = "Threshold successfully updated!"
-    @update_threshold_error_message = "Please enter a valid integer value to update!"
+    update_threshold_success_message = "Threshold successfully updated!"
+    update_threshold_error_message = "Please enter a valid integer value to update!"
     if !(global_threshold == (params[:variant][:local_threshold].to_i))
       @variant.update(variant_params)
     else
       @variant.update(local_threshold: nil)
     end
     respond_to do |format|
-      format.js
+      format.js { render "update.js.erb", :locals => {:update_threshold_success_message => update_threshold_success_message, :update_threshold_error_message => update_threshold_error_message} }
     end   
   end
 
